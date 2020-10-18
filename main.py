9ea866ccaa1907bf29943c521d1a7d517fbb4981
niravjain98 <niravjain98@gmail.com>
2020-10-18 19:05:44 +0530

    Removed unnnecessary modules and merged function into functions.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import helper
+from functions import *
 
 
 #Tune these parameters
@@ -25,87 +26,9 @@ keep_prob = tf.placeholder(tf.float32)
 
 #FUNCTIONS
 
-def load_vgg(sess, vgg_path):
 
-  # load the model and weights
-  model = tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
 
-  # Get Tensors to be returned from graph
-  graph = tf.get_default_graph()
-  image_input = graph.get_tensor_by_name('image_input:0')
-  keep_prob = graph.get_tensor_by_name('keep_prob:0')
-  layer3 = graph.get_tensor_by_name('layer3_out:0')
-  layer4 = graph.get_tensor_by_name('layer4_out:0')
-  layer7 = graph.get_tensor_by_name('layer7_out:0')
-
-  return image_input, keep_prob, layer3, layer4, layer7
-
-def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, NUMBER_OF_CLASSES):
-   
-    # Use a shorter variable name for simplicity
-    layer3, layer4, layer7 = vgg_layer3_out, vgg_layer4_out, vgg_layer7_out
-
-    # Apply 1x1 convolution in place of fully connected layer
-    fcn8 = tf.layers.conv2d(layer7, filters=NUMBER_OF_CLASSES, kernel_size=1, name="fcn8")
-
-    # Upsample fcn8 with size depth=(4096?) to match size of layer 4 so that we can add skip connection with 4th layer
-    fcn9 = tf.layers.conv2d_transpose(fcn8, filters=layer4.get_shape().as_list()[-1],
-    kernel_size=4, strides=(2, 2), padding='SAME', name="fcn9")
-
-    # Add a skip connection between current final layer fcn8 and 4th layer
-    fcn9_skip_connected = tf.add(fcn9, layer4, name="fcn9_plus_vgg_layer4")
-
-    # Upsample again
-    fcn10 = tf.layers.conv2d_transpose(fcn9_skip_connected, filters=layer3.get_shape().as_list()[-1],
-    kernel_size=4, strides=(2, 2), padding='SAME', name="fcn10_conv2d")
-
-    # Add skip connection
-    fcn10_skip_connected = tf.add(fcn10, layer3, name="fcn10_plus_vgg_layer3")
-
-    # Upsample again
-    fcn11 = tf.layers.conv2d_transpose(fcn10_skip_connected, filters=NUMBER_OF_CLASSES,
-    kernel_size=16, strides=(8, 8), padding='SAME', name="fcn11")
-
-    return fcn11
-
-def optimize(nn_last_layer, correct_label, learning_rate, NUMBER_OF_CLASSES):
-  
-  # Reshape 4D tensors to 2D, each row represents a pixel, each column a class
-  logits = tf.reshape(nn_last_layer, (-1, NUMBER_OF_CLASSES), name="fcn_logits")
-  correct_label_reshaped = tf.reshape(correct_label, (-1, NUMBER_OF_CLASSES))
-
-  # Calculate distance from actual labels using cross entropy
-  cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label_reshaped[:])
-  # Take mean for total loss
-  loss_op = tf.reduce_mean(cross_entropy, name="fcn_loss")
-
-  # The model implements this operation to find the weights/parameters that would yield correct pixel labels
-  train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss_op, name="fcn_train_op")
-
-  return logits, train_op, loss_op
-
-def train_nn(sess, epochs, batch_size, get_batches_fn, train_op,
-             cross_entropy_loss, input_image,
-             correct_label, keep_prob, learning_rate):
-
-  keep_prob_value = 0.5
-  learning_rate_value = 0.001
-  for epoch in range(epochs):
-      # Create function to get batches
-      total_loss = 0
-      for X_batch, gt_batch in get_batches_fn(batch_size):
-
-          loss, _ = sess.run([cross_entropy_loss, train_op],
-          feed_dict={input_image: X_batch, correct_label: gt_batch,
-          keep_prob: keep_prob_value, learning_rate:learning_rate_value})
-
-          total_loss += loss;
-
-      print("EPOCH {} ...".format(epoch + 1))
-      print("Loss = {:.3f}".format(total_loss))
-      print()
-
-def run():
+def main():
   
   # Download pretrained vgg model
   #helper.maybe_download_pretrained_vgg(data_dir)
@@ -134,12 +57,12 @@ def run():
     print("Model about to train, Nigga")
 
     # Train the neural network
-    train_nn(session, EPOCHS, BATCH_SIZE, get_batches_fn, 
-             train_op, cross_entropy_loss, image_input,
-             correct_label, keep_prob, learning_rate)
+    # #train_nn(session, EPOCHS, BATCH_SIZE, get_batches_fn, 
+    #          train_op, cross_entropy_loss, image_input,
+    #          correct_label, keep_prob, learning_rate)
 
-    # Run the model with the test images and save each painted output image (roads painted green)
-    helper.save_inference_samples(runs_dir, data_dir, session, IMAGE_SHAPE, logits, keep_prob, image_input)
+    # # Run the model with the test images and save each painted output image (roads painted green)
+    # helper.save_inference_samples(runs_dir, data_dir, session, IMAGE_SHAPE, logits, keep_prob, image_input)
     
     print("All done!")
 
@@ -147,13 +70,7 @@ def run():
 # MAIN
 #--------------------------
 if __name__ == '__main__':
-    run()
+    main()
 
 #FUNCTIONS
 
-def load_vgg(sess, vgg_path):
-
-    model = tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
-
-    graph = tf.get_default_graph()
-    image_input = graph.get_tensor_
